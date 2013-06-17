dbfe0ba1600470250bd6618b88d218889b817394
Rainer Müller <rainer@websimple.be>
2013-06-17 10:18:35 +0200

    pa défaut afficher le titre de la branche comme h1, pour les cq, le titre de la branche + le titre de l'enfant

---
+++ squelettes/lang/local_nl.php
@@ -0,0 +1,24 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/gis/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+    //C
+    'choisir_annee'=>'Choisissez une année',        
+    'choisir_cq'=>'Choisissez un contrat de quartier',    
+    'choisir_op'=>'Choisissez une opération',  
+    'contrat_quartier_durable'=>'Duurzaam wijkcontract',  
+        
+      
+    
+    //D
+    'date_debut'=>'Date début',
+    'date_fin'=>'Date fin',
+
+    //L
+    'lire_suite'=>'Lire la suite',
+);
+
+?>
