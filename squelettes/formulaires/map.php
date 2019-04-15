3bc1eee26874a5dbf08184321560a96c237111e6
Rainer <rainer@websimple.be>
2019-04-15 12:28:28 +0200

    permimetre pré sélectionné

--- squelettes/formulaires/map.php
+++ squelettes/formulaires/map.php
@@ -3,7 +3,9 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 function formulaires_map_charger_dist(){
-    $valeurs=array('cq'=>'','op'=>'','annee'=>'');
-    return $valeurs;
+	$valeurs = [
+		'cq' => _request('cq'),
+		'op' => _request('op'),
+		'annee' => _request('annee')];
+	return $valeurs;
 }
-?>
